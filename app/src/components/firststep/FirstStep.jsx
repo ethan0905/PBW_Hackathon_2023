f45e16fdb0322078f6e144bc84d895821574f968
Sleleu <leleu.sebastien@hotmail.fr>
2023-03-19 05:32:05 +0100

    payconsent

--- app/src/components/firststep/FirstStep.jsx
+++ app/src/components/firststep/FirstStep.jsx
@@ -19,11 +19,11 @@ function ProgressMobileStepper() {
   
     return (
       <MobileStepper
-        variant="dots"
+        variant="progress"
         steps={3}
         position="bottom"
         activeStep={activeStep}
-        sx={{ maxWidth: 400, flexGrow: 29 }}
+        sx={{ maxWidth: 400, flexGrow: 1 }}
       />
     );
   }
@@ -33,14 +33,29 @@ function ProgressMobileStepper() {
     return (
         <Button variant="contained"
         position="bottom"
+        style={{ backgroundColor: "white",
+        color: "black",
+        fontFamily: "Poppins, sans-serif",
+        position: "relative",
+        bottom: "0",
+        right: "0",
+        margin: "0",
+        textTransform: "lowercase"
+        }}
         >Next</Button>
     )
   }
 
 const FirstStep = () => {
     return (
-      // <html>
-        <div>
+       <html>
+        <header>
+            <title>Payconsent</title>
+        </header>
+        <body>
+        <div className="bg-login">
+            <p className="title-style">PAYCONSENT</p>
+            <div className="text-style">
             <form>
             <h1>
                 A solution for a smarter contract
@@ -51,8 +66,10 @@ const FirstStep = () => {
                 <NextButton />
             </h4>
             </form>
+         </div>
         </div>
-      // </html>
+        </body>
+        </html>
     );
     }
 
